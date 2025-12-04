7151f3846ad0c89c213afb6803966484a6dd76e0
Johan Edstedt <johan.edstedt@liu.se>
2025-12-04 10:01:08 +0100

    Remove model.match call from smoke test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -6,10 +6,6 @@ from romav2.device import device
 def test_smoke():
     model = RoMaV2(RoMaV2.Cfg(compile=False))
     model.apply_setting("turbo")
-    model.match(
-        torch.randn(1, 3, 320, 320).to(device), torch.randn(1, 3, 320, 320).to(device)
-    )
-
 
 if __name__ == "__main__":
     test_smoke()
